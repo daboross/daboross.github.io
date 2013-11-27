ca0e00d679962abec30299ca2ab7df4febb354e8
Dabo Ross <daboross@daboross.net>
2013-11-26 23:59:57 -0800

    JSON

--- jstest.js
+++ jstest.js
@@ -1,31 +1,30 @@
-text_map = {
-    16: "Inner circle",
-    53: "First circle",
-    85: "Second circle",
-    116: "Third circle",
-    146: "Fourth circle",
-    196: "Fifth circle"
-};
 
+var text_map;
+var outside;
+var req = new XMLHttpRequest();
+req.open("GET", "jstest.json", true);
+req.responseType = "text";
+req.onload = function() {
+    var response = JSON.parse(this.response);
+    text_map = response["circles"];
+    outside = response["other"];
+};
+req.send();
 function mouseMoveFunction(event) {
     var x = event.clientX - 8;
     var y = event.clientY - 8;
-//    print_thing("x", "x = " + x);
-//    print_thing("y", "y = " + y);
-    for (var i = 0; i < 500; i++) {
-        if (isInsideCircle(200 - x, 200 - y, i)) {
-            print_thing("radius", "Min radius is " + i);
-            break;
-        }
-    }
-    Object.keys(text_map).some(function(key) {
+    if (!Object.keys(text_map).some(function(key) {
+        key = parseInt(key);
         if (isInsideCircle(200 - x, 200 - y, key)) {
             print_thing("Text", text_map[key]);
             return true;
         }
         return false;
-    });
+    })) {
+        print_thing("Text", outside);
+    }
 }
+
 function print_thing(name, thing) {
     var parent = document.getElementById("output");
     var child = document.getElementById(name);
@@ -36,6 +35,15 @@ function print_thing(name, thing) {
     }
     child.innerHTML = thing;
 }
+
 function isInsideCircle(x, y, radius) {
     return Math.pow(x, 2) + Math.pow(y, 2) < Math.pow(radius, 2);
 }
+/*
+ for (var i = 0; i < 300; i++) {
+ if (isInsideCircle(200 - x, 200 - y, i)) {
+ print_thing("radius", "Min radius is " + i);
+ break;
+ }
+ }
+ */
