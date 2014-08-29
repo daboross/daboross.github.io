04fe86f4a4139d77cd3b18b5b3edda7eb831c726
Dabo Ross <daboross@daboross.net>
2014-08-29 13:33:39 -0800

    Initial Commit - This branch contains content generated by https://github.com/daboross/dabo.guru

---
+++ assets/contact.js
@@ -0,0 +1,3 @@
+function sendSuccess(data,textStatus,jqXHR){var $value=$("#send-value");$value.show();$value.html("Success!")}
+function sendFail(data,textStatus,jqXHR){var $value=$("#send-value");$value.show();$value.html("Failure: "+textStatus);}
+function sendData(){var sendForm=$("#send-form");var request=$.ajax({url:"/notify",type:"POST",data:sendForm.val(),complete:sendSuccess,contentType:"text/plain;charset=UTF-8"});request.done(sendSuccess);request.fail(sendFail);sendForm.hide();$("#send-button").hide();var sendValue=$("#send-value");sendValue.show();}
